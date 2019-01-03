aedde57363db8852612fc8135f7558b7b8293efd
PearFL <451159561@qq.com>
2019-01-03 14:14:00 +0800

    start project

--- test8.sql
+++ test8.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: 2019-01-03 02:57:08
+-- Generation Time: 2019-01-03 06:13:14
 -- 服务器版本： 5.7.21
 -- PHP Version: 5.6.35
 
@@ -77,6 +77,28 @@ CREATE TABLE IF NOT EXISTS `tb_customer` (
   `customer_mobile` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `t_customer`
+--
+
+DROP TABLE IF EXISTS `t_customer`;
+CREATE TABLE IF NOT EXISTS `t_customer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `jobs` varchar(20) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `t_customer`
+--
+
+INSERT INTO `t_customer` (`id`, `username`, `jobs`, `phone`) VALUES
+(1, '123', '123', '123');
 
 --
 -- 限制导出的表
